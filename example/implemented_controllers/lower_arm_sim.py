731dd801293e832d2d33ea98c6dd541f0859eac9
RodsCoimbra <rodscoimbra@gmail.com>
2026-03-13 15:57:39 +0000

    feat: refactor lower arm controller to improve logging and remove unused code

--- example/implemented_controllers/lower_arm_sim.py
+++ example/implemented_controllers/lower_arm_sim.py
@@ -18,6 +18,7 @@ import time
 from rclpy.node import Node
 import rclpy
 import numpy as np
+import logging
 SLEEP_TIME = 0.05
 
 parser = argparse.ArgumentParser()
@@ -26,9 +27,10 @@ parser.add_argument("--net", type=str, default="127.0.0.1", help="Network interf
 
 args = parser.parse_args()
    
-class Controller(Node):
+class Controller():
     def __init__(self):
-        super().__init__("controller_node")
+        logging.basicConfig(level=logging.INFO)
+        self.logger = logging.getLogger(__name__)
         self._init_communication()
         self._init_motors()
         self._cleanup_done = False
@@ -48,63 +50,67 @@ class Controller(Node):
 
     def _init_communication(self) -> None:
         try:
-            self.low_state_subscriber = B1LowStateSubscriber(self._low_state_handler)
             self.client = B1LocoClient()
             self.low_cmd_publisher = B1LowCmdPublisher()
             self.client.Init()
-            self.low_state_subscriber.InitChannel()
             self.remoteControl = RemoteControlService()
             self.low_cmd_publisher.InitChannel()
-            self.joystick_timer = self.create_timer(0.05, self.run_joystick)
             time.sleep(2)  # Wait for channels to initialize
             print("Initialization complete.")
         except Exception as e:
-            self.get_logger().error(f"Failed to initialize communication: {e}")
+            self.logger.error(f"Failed to initialize communication: {e}")
             raise
         
     def start_walking_mode(self):
         """Switch the robot to walking mode safely. If already in walking mode, does nothing.
         If not, first switches to prepare mode, waits for it to initialize and then switches to walking mode."""
-        current_mode = GetModeResponse()
-        pitch_head = 0.2
-        if self.client.GetMode(current_mode):
-            raise RuntimeError("Could not get current robot mode.")
-        
-        if current_mode.mode == RobotMode.kWalking:
-            self.get_logger().info("Robot is already in walking mode. Sending zero velocity command to ensure safe state.")
-            # self.client.Move(0.0, 0.0, 0.0)  # Send zero velocity command to ensure safe state
-
-        else:
-            self.get_logger().info("Robot is not in walking mode. Switching to prepare mode first.")
-            ret = self.client.ChangeMode(RobotMode.kPrepare)   
-            print(f"Switched to prepare mode: {ret}")
-            time.sleep(5.0) # Wait for prepare mode to initialize
-            print(f"{self.remoteControl.get_walk_operation_hint()}")
-            while not self.remoteControl.start_walk():
-                time.sleep(0.1)
-            mode = RobotMode.kWalking
-            ret=self.client.ChangeMode(mode)
-            print(f"Switched to {mode} mode: {ret}")
-            time.sleep(3.0)
-        self.client.RotateHead(pitch_head, 0.0)
-        print(f"{self.remoteControl.get_operation_hint()}")
-        
-        ## Lower arms in walking mode
+        self.dof_pos = [5.29376982e-01, -1.14427970e+00, 5.78152756e-02, -1.62383859e+00,
+                        -9.41602490e-04, 4.87757519e-02, -1.33707554e-02, 5.33520092e-01,
+                        1.14729283e+00, 4.57625798e-02, 1.60312298e+00, 1.31824349e-03,
+                        -3.29565910e-02, 1.18641914e-02]
+        controlled_j = [JOINT_PARAMETERS[name] for name in ARM_NAMES]
+        for i, joint in enumerate(controlled_j):
+                motor_idx = joint["idx"]
+                self.low_cmd.motor_cmd[motor_idx].q = self.dof_pos[i]
+                self.low_cmd.motor_cmd[motor_idx].dq = 0.0
+                self.low_cmd.motor_cmd[motor_idx].tau = 0.0
+                self.low_cmd.motor_cmd[motor_idx].kp = joint["kp"]
+                self.low_cmd.motor_cmd[motor_idx].kd = joint["kd"]
+                self.low_cmd.motor_cmd[motor_idx].weight = 1.0
+        self.low_cmd_publisher.Write(self.low_cmd)
+        input("Press Enter to switch to upper body custom control mode...")
+        self.client.UpperBodyCustomControl(True)
+        # self.logger.info("Switching to prepare mode.")
+        # ret = self.client.ChangeMode(RobotMode.kPrepare)   
+        # print(f"Switched to prepare mode: {ret}")
+        # time.sleep(5.0) # Wait for prepare mode to initialize
+        # while input("Press Enter to switch to walking mode...") != "":
+        #     time.sleep(0.1)
+        # mode = RobotMode.kWalking
+        # ret=self.client.ChangeMode(mode)
+        # print(f"Switched to {mode} mode: {ret}")
+        # time.sleep(3.0)
+        # pitch_head = 0.2
+        # self.client.RotateHead(pitch_head, 0.0)
+        # print(f"{self.remoteControl.get_operation_hint()}")
         self.low_arm()
         
 
     def low_arm(self):
         final_position = [0.0, -1.25, 0, -0.5, 0, 0, 0, 0.0, 1.25, 0, 0.5, 0, 0, 0,] 
         controlled_j = [JOINT_PARAMETERS[name] for name in ARM_NAMES]
-        self.dof_pos2 = [0.46215763688087463, -1.0906621217727661, 0.01773861236870289,
-        -1.4627679586410522, -0.00133514404296875, 0.016975661739706993, -0.00591278076171875,
-        0.465209424495697, 1.083414077758789, 0.01926451548933983, 1.4574273824691772, 
-        0.00095367431640625, -0.01506828423589468, 0.00514984130859375]
+        print(f"Controlled joints: {controlled_j}")
+        print(len(controlled_j), len(final_position))
+        self.dof_pos = [5.29376982e-01, -1.14427970e+00, 5.78152756e-02, -1.62383859e+00,
+                        -9.41602490e-04, 4.87757519e-02, -1.33707554e-02, 5.33520092e-01,
+                        1.14729283e+00, 4.57625798e-02, 1.60312298e+00, 1.31824349e-03,
+                        -3.29565910e-02, 1.18641914e-02]
                         
             
-        positions = np.array([np.linspace(self.dof_pos2[x], 
+        positions = np.array([np.linspace(self.dof_pos[x], 
                                           final_position[x], num=80) for x in range(len(controlled_j))]).swapaxes(1, 0)
-        print(self.dof_pos2)
+        print(self.dof_pos)
+        print(positions[-1])
         input("Press Enter to lower arms...")
         cmd_idx = 0
         while cmd_idx < positions.shape[0]:
@@ -123,45 +129,16 @@ class Controller(Node):
         time.sleep(0.5)
         return
     
-    def run_joystick(self):
-        if self.remoteControl.send_stop():
-            self.turn_off_arms()
-    
-    def turn_off_arms(self):
-        arms = [JOINT_PARAMETERS[name] for name in ARM_NAMES]
-        for _, joint in enumerate(arms):
-            motor_idx = joint["idx"]
-            self.low_cmd.motor_cmd[motor_idx].q = 0.0
-            self.low_cmd.motor_cmd[motor_idx].dq = 0.0
-            self.low_cmd.motor_cmd[motor_idx].tau = 0.0
-            self.low_cmd.motor_cmd[motor_idx].kp = 0.0
-            self.low_cmd.motor_cmd[motor_idx].kd = 3.0
-            self.low_cmd.motor_cmd[motor_idx].weight = 1.0
-
-    def _low_state_handler(self, low_state_msg: LowState):
-        for i, motor in enumerate(low_state_msg.motor_state_parallel):
-            self.dof_pos[i] = motor.q
-            self.dof_vel[i] = motor.dq
-    
     def cleanup(self) -> None:
         """Clean up resources (idempotent)."""
         if self._cleanup_done:
             return
         self._cleanup_done = True
 
-        self.get_logger().info("Doing cleanup...")
-        
-        # close communications
-        try:
-            self.remoteControl.close()
-        except Exception as e:
-            self.logger.error(f"Error closing remote control: {e}")
-
-        if rclpy.ok():
-            rclpy.shutdown()
-
-        self.get_logger().info("Cleanup complete")
-        
+        """Cleanup resources."""
+        self.remoteControlService.close()
+        if hasattr(self, "low_cmd_publisher"):
+            self.low_cmd_publisher.CloseChannel()
         if hasattr(self, "low_state_subscriber"):
             self.low_state_subscriber.CloseChannel()
                
@@ -172,11 +149,8 @@ class Controller(Node):
 def main():
     print(f"Starting custom controller, connecting to {args.net} ...")
     ChannelFactory.Instance().Init(0, args.net)
-    rclpy.init()
     controller = Controller()
-    rclpy.spin(controller)
     controller.start_walking_mode()
-    rclpy.shutdown()
     
 if __name__ == "__main__":
     main()
